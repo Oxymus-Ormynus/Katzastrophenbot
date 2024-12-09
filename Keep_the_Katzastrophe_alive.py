51358e5faed90161d8cb700ca29a469552dbd4a4
Oxymus <chris.oxy@hotmail.com>
2024-12-09 21:45:59 +0100

    Add files via upload

---
+++ Keep_the_Katzastrophe_alive.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+# Initialisiere die Flask-Anwendung
+app = Flask ('')
+
+# Definiere die Route für die Hauptseite
+@app.route('/')
+def home():
+    return "Katzastrophe is running!" # Angezeigte Nachricht bei jedem Ping
+    
+# Flask-Server starten
+def run ():
+    app.run(host='0.0.0.0', port=8080)
+ 
+# Server im separaten Thread ausführen
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
